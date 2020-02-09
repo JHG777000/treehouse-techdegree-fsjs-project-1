d534611d6fda4df3976779869d77d02b8574b06b
JHG777000 <jhg777000@gmail.com>
2020-02-09 15:00:39 -0500

    add getRandomQuote, setColor
    add getRandomQuote, setColor
    use setInterval to set interval for printQuote
    finish printQuote

--- js/script.js
+++ js/script.js
@@ -11,26 +11,37 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+var lastquote = null;
+var lastcolor = null;
+
 var quotes = [
     {
-      quote: "test quote",
-      source: "test source",
+      quote: "test quote1",
+      source: "test source1",
+      citation: "citation1",
+      category: 'none',
+      year: 20635
+
     },
     {
-      quote: "test quote",
-      source: "test source",
+      quote: "test quote2",
+      source: "test source2",
+      category: 'humor',
     },
     {
-      quote: "test quote",
-      source: "test source",
+      quote: "test quote3",
+      source: "test source3",
+      category: 'business',
     },
     {
-      quote: "test quote",
-      source: "test source",
+      quote: "test quote4",
+      source: "test source4",
+      category: 'business',
     },
     {
-      quote: "test quote",
-      source: "test source",
+      quote: "test quote5",
+      source: "test source5",
+      category: 'politics',
     },
 ];
 
@@ -40,16 +51,58 @@ var quotes = [
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
+ var rand = Math.floor(Math.random() * quotes.length);
+ return quotes[rand];
+}
 
+function setColor() {
+  var colors = [
+    'red',
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'silver',
+    'cyan',
+    'magenta'
+  ];
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  if ( lastcolor !== null) {
+    while ( color === lastcolor ) {
+     color = colors[Math.floor(Math.random() * colors.length)];
+   }
+  }
+  document.body.style.background = color;
+  lastcolor = color;
+}
 
 /***
  * `printQuote` function
 ***/
 function printQuote() {
+  var quote = getRandomQuote();
+  var string = " ";
+  if ( lastquote !== null) {
+   while ( quote === lastquote ) {
+    quote = getRandomQuote();
+  }
+ }
+  
+ string += '<p class="quote">'  + quote.quote + '</p>';
+ string += '<p class="source">'  + quote.source;
 
-  alert(quotes[0].quote);
+ if (quote.citation) string += '<span class="citation">'  + quote.citation;
+ if (quote.year) string += '</span>' + '<span class="year">'  + quote.year;
+ if (quote.category) string += '</span>' + '<span class="year">'  + quote.category;
+ if (quote.citation) string += '</span>';
+ string += '</p>';
+ document.getElementById('quote-box').innerHTML = string;
+ setColor();
+ lastquote = quote;
 }
 
+setInterval(function(){ printQuote(); }, 10000);
 
 /***
  * click event listener for the print quote button

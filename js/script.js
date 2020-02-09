1a3b4552b041f7cb0d72189b9da3df8728b7e4b1
JHG777000 <jhg777000@gmail.com>
2020-02-09 17:46:19 -0500

    add comments

--- js/script.js
+++ js/script.js
@@ -3,16 +3,17 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
+/* 
+  the 'last' vars, use make sure color and quotes are not
+  same in a row
+*/
 
 var lastquote = null;
 var lastcolor = null;
+
+/* 
+  quotes array - the array that stores all the quotes
+*/
 
 var quotes = [
     {
@@ -63,16 +64,20 @@ var quotes = [
     
 ];
 
-
-
-/***
- * `getRandomQuote` function
-***/
+/* 
+  getRandomQuote function - the function that selects
+  a random quote
+*/
 
 function getRandomQuote() {
  var rand = Math.floor(Math.random() * quotes.length);
  return quotes[rand];
 }
+
+/* 
+  setColor function - the function that selects
+  a random background color
+*/
 
 function setColor() {
   var colors = [
@@ -87,39 +92,51 @@ function setColor() {
   ];
   var color = colors[Math.floor(Math.random() * colors.length)];
   if ( lastcolor !== null) {
+    //loop until different from last color
     while ( color === lastcolor ) {
      color = colors[Math.floor(Math.random() * colors.length)];
    }
   }
   document.body.style.backgroundColor = color;
-  lastcolor = color;
+  lastcolor = color; //set lastcolor to the current color
 }
 
-/***
- * `printQuote` function
-***/
+/* 
+  printQuote function - the function that prints
+  a quote from getRandomQuote, with formatting for
+  each object property
+*/
+
 function printQuote() {
+  var HTMLstring = " ";
   var quote = getRandomQuote();
-  var string = " ";
+
   if ( lastquote !== null) {
+   //loop until different from last quote
    while ( quote === lastquote ) {
     quote = getRandomQuote();
   }
  }
-  
- string += '<p class="quote">'  + quote.quote + '</p>';
- string += '<p class="source">'  + quote.source;
 
- if (quote.citation) string += '<span class="citation">'  + quote.citation;
- if (quote.year) string += '</span>' + '<span class="year">'  + quote.year;
- if (quote.category) string += '</span>' + '<span class="year">'  + quote.category;
- if (quote.citation) string += '</span>';
- string += '</p>';
- document.getElementById('quote-box').innerHTML = string;
- setColor();
- lastquote = quote;
+ /*
+  set up the HTML string that will display the quote
+  and, format each object property
+ */
+
+HTMLstring += '<p class="quote">'  + quote.quote + '</p>';
+HTMLstring += '<p class="source">'  + quote.source;
+ if (quote.citation) HTMLstring += '<span class="citation">'  + quote.citation;
+ if (quote.year) HTMLstring += '</span>' + '<span class="year">'  + quote.year;
+ if (quote.category) HTMLstring += '</span>' + '<span class="year">'  + quote.category;
+ if (quote.citation || quote.year || quote.category) HTMLstring += '</span>'; //add the final </span> if quote uses extra properties 
+ HTMLstring += '</p>'; //add the final </p>
+
+ document.getElementById('quote-box').innerHTML = HTMLstring; //use HTML string
+ setColor(); //change background color at random
+ lastquote = quote; //set lastquote to the current quote
 }
 
+//use setInterval to call printQuote, every 10 sec
 setInterval(function(){ printQuote(); }, 10000);
 
 /***
